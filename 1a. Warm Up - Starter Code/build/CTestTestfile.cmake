b534c627aab28256f6d0a1b73424076d2a7c350b
Agustin Fisher <afisher@itba.edu.ar>
2022-03-08 21:02:11 -0300

    removed build

--- 1a. Warm Up - Starter Code/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nomad1c/itba/eda/Level1/1a. Warm Up - Starter Code
-# Build directory: /home/nomad1c/itba/eda/Level1/1a. Warm Up - Starter Code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "/home/nomad1c/itba/eda/Level1/1a. Warm Up - Starter Code/build/orbitalsim_test")
-set_tests_properties(test1 PROPERTIES  _BACKTRACE_TRIPLES "/home/nomad1c/itba/eda/Level1/1a. Warm Up - Starter Code/CMakeLists.txt;33;add_test;/home/nomad1c/itba/eda/Level1/1a. Warm Up - Starter Code/CMakeLists.txt;0;")
